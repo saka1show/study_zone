5a828070e7dc788a8a5bc6d063b049ba81b309af
EC2 Default User <ec2-user@ip-172-31-27-47.ap-northeast-1.compute.internal>
2024-04-15 11:08:23 +0000

    不具合修正4

--- app/controllers/admin/learners_controller.rb
+++ app/controllers/admin/learners_controller.rb
@@ -8,7 +8,7 @@ class Admin::LearnersController < ApplicationController
     @learner = Learner.find(params[:id])
     @created_problems = CreatedProblem.where(learner_id: @learner.id).order(created_at: :desc).page(params[:page]).per(5)
     @point_today = Point.where(learner_id: @learner.id).where("DATE_FORMAT(created_at, '%Y-%m-%d') = ?", Date.today.strftime('%Y-%m-%d')).count
-    @point_this_month = Point.where(learner_id: @learner.id).where("DATE_FORMAT(created_at, '%Y-%m-%d') = ?", Date.today.strftime('%Y-%m')).count
+    @point_this_month = Point.where(learner_id: @learner.id).where("DATE_FORMAT(created_at, '%Y-%m') = ?", Date.today.strftime('%Y-%m')).count
     @point_all = Point.where(learner_id: @learner.id).count
     @comments_sent = Comment.where(learner_id: @learner.id)
     @comments_received = Comment.joins(solved_problem: :created_problem).where(created_problems: { learner_id: @learner.id })
