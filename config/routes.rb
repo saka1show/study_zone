ee0f5a3b74ac390c6aab968a441ebcb98e687324
EC2 Default User <ec2-user@ip-172-31-27-47.ap-northeast-1.compute.internal>
2024-05-15 06:14:28 +0000

    [Update]コメント削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     resources :subjects, only: [:new, :create, :index, :edit, :update]
     resources :points, only: [:index]
     resources :created_problems, only: [:index, :show, :edit, :update, :destroy]
+    resources :comments, only: [:destroy]
   end
 
   scope module: :public do
